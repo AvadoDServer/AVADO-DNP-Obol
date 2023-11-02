09fe89cff0da7c49f43414caf1f4be5b0013dc51
sponnet <stefaan@ponnet.com>
2023-11-02 11:02:30 +0100

    mainnet + MeV boost

--- build/startTeku.sh
+++ build/startTeku.sh
@@ -18,7 +18,27 @@ until [ -d "/data/charon/validator_keys" ]; do
   sleep 30
 done
 
+# exec /opt/teku/bin/teku --version
+
+echo "Starting Teku"
+
 # Start teku
+echo "exec /opt/teku/bin/teku validator-client \
+  --beacon-node-api-endpoint="http://localhost:3600" \
+  --data-base-path="${DATA_PATH}" \
+  --network="${NETWORK}" \
+  --validator-keys="/data/charon/validator_keys:/data/charon/validator_keys" \
+  --validators-keystore-locking-enabled=false \
+  --validators-graffiti="${GRAFFITI}" \
+  --validators-proposer-default-fee-recipient="0x0000000000000000000000000000000000000000" \
+  --validator-api-cors-origins="*" \
+  --validator-api-enabled=true \
+  --validator-api-host-allowlist="*" \
+  --validator-api-interface="0.0.0.0" \
+  --validator-api-keystore-file=/opt/teku/avado.keystore \
+  --validator-api-keystore-password-file=/opt/teku/keystorePasswordFile \
+  --validator-api-port=5052"
+
 exec /opt/teku/bin/teku validator-client \
   --beacon-node-api-endpoint="http://localhost:3600" \
   --data-base-path="${DATA_PATH}" \
@@ -33,4 +53,6 @@ exec /opt/teku/bin/teku validator-client \
   --validator-api-interface="0.0.0.0" \
   --validator-api-keystore-file=/opt/teku/avado.keystore \
   --validator-api-keystore-password-file=/opt/teku/keystorePasswordFile \
-  --validator-api-port=5052
+  --validator-api-port=5052 \
+  --validators-proposer-config=http://localhost:3600/teku_proposer_config \
+  --validators-proposer-blinded-blocks-enabled=true 
